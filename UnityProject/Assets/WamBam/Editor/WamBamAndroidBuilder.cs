a0e7f197b8facf91504c0a44d1998aeeaa15355d
Macca <bigmacblu@gmail.com>
2026-08-18 20:00:42 +0100

    Fix Unity Android batch builder

--- UnityProject/Assets/WamBam/Editor/WamBamAndroidBuilder.cs
+++ UnityProject/Assets/WamBam/Editor/WamBamAndroidBuilder.cs
@@ -12,18 +12,19 @@ namespace WamBamThankUMam.Editor
         public static void BuildAndroidApk()
         {
             WamBamProjectSetup.Prepare();
+
             string outDir = Path.GetFullPath(Path.Combine(Application.dataPath, "../Builds/Android"));
             Directory.CreateDirectory(outDir);
             string outPath = Path.Combine(outDir, "WamBamThankUMam-Alpha-0.5.apk");
 
             EditorUserBuildSettings.buildAppBundle = false;
-            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
 
             BuildReport report = BuildPipeline.BuildPlayer(new BuildPlayerOptions
             {
                 scenes = new[] { "Assets/Scenes/Main.unity" },
                 locationPathName = outPath,
                 target = BuildTarget.Android,
+                targetGroup = BuildTargetGroup.Android,
                 options = BuildOptions.Development
             });
 
@@ -31,12 +32,6 @@ namespace WamBamThankUMam.Editor
                 throw new System.Exception("Android build failed: " + report.summary.result);
 
             Debug.Log("Wam Bam APK built: " + outPath);
-        }
-
-        public static void BuildAndroidFromCommandLine()
-        {
-            BuildAndroidApk();
-            EditorApplication.Exit(0);
         }
     }
 }
